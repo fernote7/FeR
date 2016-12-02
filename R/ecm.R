045182f411a4071afac832ba8fa79a2c306bf2b6
fernote7 <fernote7@gmail.com>
2016-12-02 14:52:35 -0200

    correcting the ECM function.

--- R/ecm.R
+++ R/ecm.R
@@ -10,7 +10,9 @@
 #' @examples
 #' ECM(y ~ x + z, df)
 
-ECM = function (formula, serie){
+ECM = function (formula, serie) {
+    
+    assign("formula1", formula, envir = .GlobalEnv)
     
     for (j in 1:length(attributes(serie)$class)) {
         if (attributes(serie)$class[j] != "mts" | attributes(serie)$class[j] != 
@@ -20,48 +22,40 @@ ECM = function (formula, serie){
         }
     }
     assign("tserie", serie, envir = .GlobalEnv)
-    a = dynlm(formula, data = tserie)
-    f = as.character(formula)
+    
+    a = dynlm(formula1, data = tserie)
+    f = as.character(formula1)
     f = f[2]
     f = strsplit(f, split = ",")
     f0 = substr(f[[1]][1], start = 1, stop = 2)
-
-    
     if (f0 == "d(") {
-        
         f1 = substr(f[[1]][1], start = 3, stop = 100)
-        
     } else {
-        
         f1 = substr(f[[1]][1], start = 6, stop = 100)
-        
     }
-    
     nomes = names(a$coefficients)
-    nome = NULL    
-    
+    nome = NULL
     for (i in 1:length(nomes)) {
         nome[i] = strsplit(nomes[i], split = ",")
         f3 = substr(nome[[i]][1], start = 3, stop = 100)
         if (f1 == f3) {
             f4 = nomes[i]
-            index = i
+            indice = i
         }
     }
-    
     old.coef = a$coefficients
     su = summary(a)
-    pval=list(su$coefficients[,4])
-    
+    pval = list(su$coefficients[, 4])
     for (j in 1:length(nomes)) {
-        if (j != index) {
+        if (j != indice) {
             f2 = substr(nome[[j]][1], start = 1, stop = 2)
             if (f2 == "L(") {
-                su$coefficients[j,1] = - su$coefficients[j,1]/ a$coefficients[f4]
-                a$coefficients[j] = - a$coefficients[j]/ a$coefficients[f4]
+                su$coefficients[j, 1] = -su$coefficients[j, 1]/a$coefficients[f4]
+                a$coefficients[j] = -a$coefficients[j]/a$coefficients[f4]
             }
         }
     }
-    lista = list(new.summary = su, new.coef = a, pval=pval, old.coef=old.coef)
+    lista = list(new.summary = su, new.coef = a, pval = pval, 
+                 old.coef = old.coef)
     return(invisible(lista))
 }
